62f808bafefa1ccbfedfadac998bc83060e9f098
watandost <watandost10@gmail.com>
2021-01-20 12:07:16 +0430

    First Commit

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "10:00|16:58";
+            string startTime = test.Substring(0, 5);
+            string endTime = test.Substring(6, 5);
+
+            Console.WriteLine(DateTime.Now.ToString("HH:mm"));
+
+            TimeSpan hourMinute = DateTime.Now.TimeOfDay;
+
+            TimeSpan sTime = TimeSpan.Parse(startTime);
+            TimeSpan eTime = TimeSpan.Parse(endTime);
+
+            Console.WriteLine(hourMinute);
+
+            if(hourMinute >= sTime && hourMinute <= eTime)
+            {
+                Console.WriteLine("Start Time:"+sTime);
+                Console.WriteLine("End Time:"+eTime);
+                Console.WriteLine("Time is "+hourMinute+" and it is valid to run the serivce");
+            }
+            else
+            {
+                Console.WriteLine("Service is idle");
+            }
+
+        }
+    }
+}
